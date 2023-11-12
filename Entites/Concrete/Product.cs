156c706e1574fa3ce5191ece806a6cb960e41c49
koyuncu5362 <koyuncu.m@hotmail.com>
2023-11-12 23:12:52 +0300

    Add project files.

---
+++ Entites/Concrete/Product.cs
@@ -0,0 +1,14 @@
+﻿using Entites.Abstract;
+
+namespace Entities.Concrete
+{
+    public class Product : IEntity
+    {
+        public int ProcutId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public short UnitInStock { get; set; }
+        public decimal UnitPrice { get; set; }
+
+    }
+}
